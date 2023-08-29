71eb5669b1e4ea2b150bf446e3e5f4fc04e80ee6
thomp <thomp@HAL9000>
2023-08-29 15:00:38 -0500

    Add project files.

---
+++ Test2.0/Test2.0/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test2._0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                scanResults_txt.Text = result.Text + " (type: " + result.BarcodeFormat + ")";
+            });
+
+        }
+    }
+}
