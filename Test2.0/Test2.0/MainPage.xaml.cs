356deaf73a0d10d22c85814c0aa0b87e51432c85
gnarlsley <thompson.garrett7413@gmail.com>
2023-09-04 14:52:08 -0500

    Formatting App interface, adding button functions

--- Test2.0/Test2.0/MainPage.xaml.cs
+++ Test2.0/Test2.0/MainPage.xaml.cs
@@ -17,11 +17,17 @@ namespace Test2._0
 
         private void ZXingScannerView_OnScanResult(ZXing.Result result)
         {
+            ScannerView.IsScanning = false;
             Device.BeginInvokeOnMainThread(() =>
             {
                 scanResults_txt.Text = result.Text + " (type: " + result.BarcodeFormat + ")";
             });
 
+        }
+
+        private void Scan_button_Pressed(object sender, EventArgs e)
+        {
+            ScannerView.IsScanning = true;
         }
     }
 }
